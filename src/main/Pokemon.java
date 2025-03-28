8a95f3bea40a5326168d0a46d1c46cd5b2a10cf3
Pedro-Almira <Afvgley8.53b@gmail.com>
2025-03-28 13:20:29 +0100

    Adicion del constuctor Pokemon
    A través de este commit se presenta el archivo Pokemon, diseñado como un constructor para almacenar todos los Pokémon que se utilizarán en el programa a nivel de Java (incluyendo pruebas y datos temporales). El trabajo y diseño de este archivo ha sido acordado entre todos los miembros del equipo, pero aún está sujeto a cambios.

---
+++ src/main/Pokemon.java
@@ -0,0 +1,259 @@
+package main;
+
+import java.util.Random;
+
+public class Pokemon {
+	
+	
+	/*
+	 * Pokemon basico
+	 * Sexo solo podra ser H, M, X
+	 * Estado solo podra ser paralizado, quemado, envenenado, toxico, dormido, congelado, pokerus, confuso, enamorado, drenado, maldito, canto mortal, atrapado, debilitado
+	 * Si el mote es "" el pokemon respondera por el nombre de pokedex
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
+	private String mote;
+	private int numeroDePokedex;
+	private int vitalidad;
+	private int vitalidadRestante;
+	private int ataque;
+	private int defensa;
+	private int ataqueEspecial;
+	private int defensaEspecial;
+	private int velocidad;
+	private byte nivel;
+	private byte fertilidad;
+	private char sexo;
+	private String estado;
+	private byte itemEquipado;
+	
+	private Pokemon() {
+		super();
+		this.mote = "";
+		this.numeroDePokedex = 1;
+		this.vitalidad = 1;
+		this.vitalidadRestante = 1;
+		this.ataque = 1;
+		this.defensa = 1;
+		this.ataqueEspecial = 1;
+		this.defensaEspecial = 1;
+		this.velocidad = 1;
+		this.nivel = 1;
+		this.fertilidad = 1;
+		this.sexo = 'X';
+		this.estado = "NULL";
+		this.itemEquipado = 1;
+	}
+
+	private Pokemon(String mote, int numeroDePokedex, int vitalidad, int vitalidadRestante, int ataque, int defensa,
+			int ataqueEspecial, int defensaEspecial, int velocidad, byte nivel, byte fertilidad, char sexo,
+			String estado, byte itemEquipado) {
+		this.mote = mote;
+		this.numeroDePokedex = numeroDePokedex;
+		this.vitalidad = vitalidad;
+		this.vitalidadRestante = vitalidadRestante;
+		this.ataque = ataque;
+		this.defensa = defensa;
+		this.ataqueEspecial = ataqueEspecial;
+		this.defensaEspecial = defensaEspecial;
+		this.velocidad = velocidad;
+		this.nivel = nivel;
+		this.fertilidad = fertilidad;
+		this.sexo = sexo;
+		this.estado = estado;
+		this.itemEquipado = itemEquipado;
+	}
+
+	private Pokemon(Pokemon p) {
+		this.mote = p.mote;
+		this.numeroDePokedex = p.numeroDePokedex;
+		this.vitalidad = p.vitalidad;
+		this.vitalidadRestante = p.vitalidadRestante;
+		this.ataque = p.ataque;
+		this.defensa = p.defensa;
+		this.ataqueEspecial = p.ataqueEspecial;
+		this.defensaEspecial = p.defensaEspecial;
+		this.velocidad = p.velocidad;
+		this.nivel = p.nivel;
+		this.fertilidad = p.fertilidad;
+		this.sexo = p.sexo;
+		this.estado = p.estado;
+		this.itemEquipado = p.itemEquipado;
+	}
+
+
+	public String getMote() {
+		return mote;
+	}
+
+
+
+	public void setMote(String mote) {
+		this.mote = mote;
+	}
+
+
+
+	public int getNumeroDePokedex() {
+		return numeroDePokedex;
+	}
+
+
+
+	public void setNumeroDePokedex(int numeroDePokedex) {
+		this.numeroDePokedex = numeroDePokedex;
+	}
+
+
+
+	public int getVitalidad() {
+		return vitalidad;
+	}
+
+
+
+	public void setVitalidad(int vitalidad) {
+		this.vitalidad = vitalidad;
+	}
+
+
+
+	public int getVitalidadRestante() {
+		return vitalidadRestante;
+	}
+
+
+
+	public void setVitalidadRestante(int vitalidadRestante) {
+		this.vitalidadRestante = vitalidadRestante;
+	}
+
+
+
+	public int getAtaque() {
+		return ataque;
+	}
+
+
+
+	public void setAtaque(int ataque) {
+		this.ataque = ataque;
+	}
+
+
+
+	public int getDefensa() {
+		return defensa;
+	}
+
+
+
+	public void setDefensa(int defensa) {
+		this.defensa = defensa;
+	}
+
+
+
+	public int getAtaqueEspecial() {
+		return ataqueEspecial;
+	}
+
+
+
+	public void setAtaqueEspecial(int ataqueEspecial) {
+		this.ataqueEspecial = ataqueEspecial;
+	}
+
+
+
+	public int getDefensaEspecial() {
+		return defensaEspecial;
+	}
+
+
+
+	public void setDefensaEspecial(int defensaEspecial) {
+		this.defensaEspecial = defensaEspecial;
+	}
+
+
+
+	public int getVelocidad() {
+		return velocidad;
+	}
+
+
+
+	public void setVelocidad(int velocidad) {
+		this.velocidad = velocidad;
+	}
+
+
+
+	public byte getNivel() {
+		return nivel;
+	}
+
+
+
+	public void setNivel(byte nivel) {
+		this.nivel = nivel;
+	}
+
+
+
+	public byte getFertilidad() {
+		return fertilidad;
+	}
+
+
+
+	public void setFertilidad(byte fertilidad) {
+		this.fertilidad = fertilidad;
+	}
+
+
+
+	public char getSexo() {
+		return sexo;
+	}
+
+
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+
+
+	public String getEstado() {
+		return estado;
+	}
+
+
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+
+
+	public byte getItemEquipado() {
+		return itemEquipado;
+	}
+
+
+
+	public void setItemEquipado(byte itemEquipado) {
+		this.itemEquipado = itemEquipado;
+	}
+	
+	
+	
+	protected void suvirDeNievel(Pokemon poke) {
+		Random rand = new Random();
+	}
+}
